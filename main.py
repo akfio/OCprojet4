8e16a6b22211a81b3ef0da9d59335ad92d9f8e41
akfio <74294901+akfio@users.noreply.github.com>
2021-01-27 14:07:56 +0100

    model et view

---
+++ main.py
@@ -0,0 +1,13 @@
+class Tournoi:
+    def __init__(self, name, place, date, turn, round, player, time, description):
+        self.nom = name
+        self.lieu = place
+        self.date = date
+        self.tour = turn
+        self.round = round
+        self.player = player
+        self.temps = time
+        self.description = description
+
+
+pass
---
+++ view.py
@@ -0,0 +1,28 @@
+from main import Tournoi
+
+
+
+def get_name(self):
+    name = input('Nom du Tournoi? ')
+    return name
+
+
+def get_place(self):
+    place = input('Lieu du tournoi? ')
+    return place
+
+
+def get_date():
+    try:
+        date = input('jour ')
+        return date
+    except ValueError:
+        return get_date
+
+
+
+def get_turn(self):
+    turn = input()
+
+
+get_date()
