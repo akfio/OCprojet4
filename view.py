dafd262cbcfdcd4a9eec1926d14c3876ab8e6bd9
akfio <74294901+akfio@users.noreply.github.com>
2021-02-01 17:28:10 +0100

    Update view.py

--- view.py
+++ view.py
@@ -2,28 +2,49 @@ from main import Tournoi
 
 
 
-def get_name(self):
-    name = input('Nom du Tournoi? ')
-    return name
+def get_name():
+    try:
+        name = input('Nom du Tournoi? ')
+        if len(name) > 20:
+            print('Nom du tournoi trop long')
+            return get_name()
+        return name
+    except ValueError:
+        return get_name()
 
 
-def get_place(self):
-    place = input('Lieu du tournoi? ')
-    return place
+def get_place():
+    try:
+        place = input('Lieu du tournoi? ')
+        if len(place) > 20:
+            return get_place()
+        return place
+    except ValueError:
+        return get_place()
 
 
 def get_date():
     try:
-        date = input('jour ')
-        return date
+        date1 = input('Début du tournoi: ' 'JJ/MM/AAAA: ')
+        if len(date1) != 10:
+            return get_date()
+        date2 = input('fin du tournoi: ' 'JJ/MM/AAAA: ')
+        if len(date2) != 10:
+            return get_date()
+        return print(date1 + ' au ' + date2)
     except ValueError:
-        return get_date
+        return get_date()
 
 
 
 def get_turn():
     turn = 4
-    return
+    print('nombre de tours par défault: 4 ')
+    more = input('Entrer une nouvelle valeur ou appuyer sur "ENTREE" ')
+    if len(more) > 0:
+        turn = more
+    print('nombre de tours = ' + str(turn))
+    return turn
 
 
 def get_round():
@@ -32,6 +53,30 @@ def get_round():
 
 def get_player():
     return
+
+
+def get_type():
+    try:
+        print('Taper 1 pour Bullet')
+        print('Taper 2 pour Blitz')
+        print('Taper 3 pour Rapide')
+        type = int(input('Choix = '))
+        if type == 1:
+            time = 'Bullet'
+            print('Partie Bullet')
+            return time
+        if type == 2:
+            time = 'Biltz'
+            print('Partie Blitz')
+            return time
+        if type == 3:
+            time = 'Rapide'
+            print('Partie Rapide')
+            return time
+        if type != [1, 2, 3]:
+            return get_type()
+    except ValueError:
+        return get_type()
 
 
 def get_description(self):
@@ -43,11 +88,16 @@ def get_description(self):
 
 # Participants
 def get_p_name(self):
-    name = input('Nom du joueur? ')
-    if len(name) > 20:
-        print('Nom du tournoi trop long')
-        return self.get_p_name()
-    return name
+    try:
+        name = input('Nom du joueur? ')
+        if len(name) > 20:
+            print('Nom du tournoi trop long')
+            return self.get_p_name()
+        return name
+    except ValueError:
+        return get_p_name()
+    
+
 
 
 def get_pname():
