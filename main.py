4552590799621fbe835fb07f9c1985e3c8ce8c74
akfio <74294901+akfio@users.noreply.github.com>
2021-02-12 23:07:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ class Tournoi:
         self.type = type
         self.description = description
 
-    def tournoi(self):
-        return self.nom, self.lieu, self.date, self.tour, self.round, self.player, self.type, self.description
-
 
 class Participants:
     def __init__(self, nom, prenom, age, sexe, classement):
@@ -21,26 +18,17 @@ class Participants:
         self.sexe = sexe
         self.classement = classement
 
-    def participants(self):
-        return self.nom, self.prenom, self.age, self.sexe, self.classement
-
-
-p = Participants("test", "Tes", "tre", "fr", "yth")
-for a, b in p.__dict__:
-    print(b)
-
 
 class Rounds:
     def __init__(self):
         self.lst_match = []
-    
-    
-    def Add_Match(self):
-        self.lst_match = 
-        
-        
-class Match:
+
+    def Add_Match(self, lst_matchs):
+        self.lst_match.append(lst_matchs)
+
+
+class Matches:
     def __init__(self, IDBlanc, IDNoir, Result):
-        self.P_blanc = IDBlanc
-        self.P_noir = IDNoir
+        self.P_blanc = ID_Blanc
+        self.p_noir = ID_Noir
         self.resultat = Result
